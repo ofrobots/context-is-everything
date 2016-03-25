a5b7672ab9b2656d784020364e2016442dda71a6
Ali Ijaz Sheikh <ofrobots@google.com>
2016-03-24 18:36:59 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var binders = [];
+
+module.exports = {
+  bind: function(callback) {
+    binders.forEach(function(binder) {
+      callback = binder(callback);
+    });
+  },
+
+  register: function(bindFunction) {
+    if (typeof bindFunction !== 'function') {
+      throw new Error('argument must be a function');
+    }
+    if (typeof bindFunction(function() {}) !== 'function') {
+      throw new Error('bindFunction must return a function')
+    }
+    binders.push(bindFunction);
+  },
+
+  unregister: function(bindFunction) {
+    var index = binders.indexOf(bindFunction);
+    if (index === -1) {
+      throw new Error('unable to find function to unregister');
+    }
+    binders.splice(index, 1);
+  }
+};
+
+module.exports.register(function domainBinder(callback) {
+  return process.domain ? process.domain.bind(callback) : callback;
+});
+
+// In anticipation of the Zone spec.
+// https://github.com/domenic/zones
+if (typeof Zone !== 'undefined' &&
+    typeof Zone.current === 'function' &&
+    typeof Zone.current().wrap === 'function') {
+  module.exports.register(function zoneBinder(callback) {
+    return Zone.current().wrap(callback);
+  });
+}
