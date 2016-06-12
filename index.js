1e4c740477782d6d36b0e64b9104ad36196a2f9b
Ali Ijaz Sheikh <ofrobots@google.com>
2016-06-12 14:35:51 -0700

    Add some comments

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 var binders = [];
 
 module.exports = {
+  
+  // This is the interface that would be used by modules that take
+  // callbacks and put them in their own queue; breaking async context
+  // propagation. Before putting the callback into the queue, they
+  // can call this bind function which returns a bound version of the
+  // callback (if necessary) that ensures that all registered context
+  // observers see the correct async context for this callback.
   bind: function(callback) {
     binders.forEach(function(binder) {
       callback = binder(callback);
@@ -10,6 +17,9 @@ module.exports = {
     return callback;
   },
 
+  // A context observer (e.g. continuation-local-storage, NewRelic, etc.)
+  // would use register/unregister to declare their intent to monitor
+  // async tasks. Domains or Zones, if active, are automatically handled.
   register: function(bindFunction) {
     if (typeof bindFunction !== 'function') {
       throw new Error('argument must be a function');
